cfbbcc0f626c17af710501b0bfe022c685c95953
Evan Hearne <ehearne@redhat.com>
2025-12-04 17:24:46 +0000

    fix(konnectivity-agent): remove unnecessary crb and role + test fixtures
    This change removes unecessary clusterrolebinding and role from konnectivity-agent, and removes their associated test fixtures.

--- control-plane-operator/controllers/hostedcontrolplane/v2/konnectivity_agent/component.go
+++ control-plane-operator/controllers/hostedcontrolplane/v2/konnectivity_agent/component.go
@@ -3,8 +3,6 @@ package konnectivity
 import (
 	kasv2 "github.com/openshift/hypershift/control-plane-operator/controllers/hostedcontrolplane/v2/kas"
 	component "github.com/openshift/hypershift/support/controlplane-component"
-
-	rbacv1 "k8s.io/api/rbac/v1"
 )
 
 const (
@@ -35,19 +33,5 @@ func NewComponent() component.ControlPlaneComponent {
 	return component.NewDeploymentComponent(ComponentName, &konnectivityAgent{}).
 		WithAdaptFunction(adaptDeployment).
 		WithDependencies(kasv2.ComponentName).
-		WithManifestAdapter(
-			"clusterrolebinding.yaml",
-			component.WithAdaptFunction(adaptClusterRoleBinding),
-		).
 		Build()
-}
-
-func adaptClusterRoleBinding(cpContext component.WorkloadContext, crb *rbacv1.ClusterRoleBinding) error {
-	// Set the namespace for ServiceAccount subjects
-	for i := range crb.Subjects {
-		if crb.Subjects[i].Kind == "ServiceAccount" {
-			crb.Subjects[i].Namespace = cpContext.HCP.Namespace
-		}
-	}
-	return nil
 }
